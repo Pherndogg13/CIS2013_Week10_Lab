89f9c18170847b9301f69bc927a5431d06858a0b
Pherndogg13 <46982716+Pherndogg13@users.noreply.github.com>
2019-03-27 19:46:33 -0700

    Add files via upload

---
+++ CIS2013_Week10_Lab/Person.cpp
@@ -0,0 +1,52 @@
+#include <string>
+
+using namespace std;
+
+class Person {
+
+	private:
+	
+		string name;
+		string gender;
+		int age;
+		string race;
+		bool is_alive;
+	
+	public:
+	
+		Person(){
+			setName("unknown");
+			setGender("M");
+			setRace("ugly");
+			setAge(age);
+		}
+		
+		Person(string n, string g, string r){
+			setName(n);
+			setGender(g);
+			setRace(r);
+			setAge(age);
+		}
+	
+		string getStats(){
+			return "Name: " + name + "\n" + "  Gender: " + gender + "\n" + "  Race: " + race;
+		}
+	
+		string getName(){ return name; }
+		string getGender(){ return gender; }
+		int getAge(){ return age; }
+		string getRace(){ return race; }
+		bool getAlive(){ return is_alive; }
+	
+		void setName(string n){ name = n; }
+		void setGender(string g){ gender = g; }
+		void setRace(string r){ race = r; }
+		void setAlive(bool a){ is_alive = a; }
+		void setAge(int a){ 
+			if( (0 > a) || (150<a) ){
+				age = 25;
+			} else {
+				age = a; 
+			}
+		}
+};
---
+++ CIS2013_Week10_Lab/main.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+#include <string>
+#include "Person.cpp"
+
+using namespace std;
+
+int main(){
+	
+	Person bob;
+	Person tom("Tom","M","stupid");
+	Person p3("Susan","F","pretty");
+	Person mypeople[10];
+	
+	cout << bob.getStats() << endl;
+	cout << "   Age: " << bob.getAge() << endl;
+	cout << tom.getStats() << endl;
+	cout << "   Age: " << tom.getAge() << endl;
+	cout << p3.getStats() << endl;
+	cout << "   Age: " << p3.getAge() << endl;
+	
+	bob.setName("Bob");
+	bob.setAge(1000);
+	cout << "   Age: " << bob.getAge() << endl;
+	bob.setRace("Obnoxious");
+	bob.setAlive(true);
+	bob.setGender("Male");
+	
+	cout << bob.getStats() << endl;
+
+	return 0;
+}
